95581a02bf75ac7dabbdb3176dce33b0e19382db
Leny <lenymrq@gmail.com>
2026-04-15 16:04:27 +0200

    feat: credit card number encryption

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
@@ -22,16 +22,13 @@ public class CreditCardConverter implements AttributeConverter<String, String> {
 
     @Override
     public String convertToDatabaseColumn(String attribute) {
-        // Step 7a: Encrypt the PAN before storing it in the database
-        return attribute;
-        // Step 7a: End of PAN encryption
+        String encrypted = CryptographyHelper.encryptData(attribute);
+        return encrypted;
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
-        // Step 7b: Decrypt the PAN when reading it from the database
-        String pan = dbData;
-        // Step 7b: End of PAN decryption
+        String pan = CryptographyHelper.decryptData(dbData);
         String maskedPanString = panMasking(pan);
         return maskedPanString;
     }
